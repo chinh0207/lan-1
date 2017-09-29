adbf2ec9b0f6d4d7e235ca6a769d601082888740
chinh0207 <32379968+chinh0207@users.noreply.github.com>
2017-09-29 16:17:32 +0700

    Add files via upload

---
+++ helloWord.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class helloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World. Welcome to");
+		System.out.print("Tutorial By chinh");  
+
+	}
+
+}
